5b2d13292e14ccf764fd5216c6b7a7903e5ecd0f
Ivan Markin <ivan.a.markin@gmail.com>
2023-01-15 18:25:24 +0600

    All done

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,14 @@ import * as flsFunctions from "./modules/functions.js";
 import smoothscroll from 'smoothscroll-polyfill';
 
 const sortSelectBtn = document.querySelector('#sortSelectBtn');
+const burgerBtn = document.querySelector('.mobile-menu-btn');
+const mobileMenuClose = document.querySelector('.mobile-menu__close');
 
 flsFunctions.isWebp();
 smoothscroll.polyfill();
+flsFunctions.newsBlockWidth();
 
 sortSelectBtn.addEventListener('click', flsFunctions.selectHandler);
 window.addEventListener('resize', flsFunctions.newsBlockWidth);
+burgerBtn.addEventListener('click', flsFunctions.mobileMenuHandler().open);
+mobileMenuClose.addEventListener('click', flsFunctions.mobileMenuHandler().close);
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -25,21 +25,21 @@ export function newsBlockWidth() {
   root.style.setProperty('--newsLeftPosition', wrapper.offsetWidth / 2 - 40 + "px");    
 }
 
-// export const mobileMenuHandler = () => {
-// 	const mobileMenuPopup = document.querySelector('.mobile-menu');
+export const mobileMenuHandler = () => {
+	const mobileMenuPopup = document.querySelector('.mobile-menu');
 
-// 	const openMenu = () => {
-// 		mobileMenuPopup?.classList.add('active');
-// 		document.body.classList.add('fixed');
-// 	}
+	const openMenu = () => {
+		mobileMenuPopup.classList.add('active');
+		document.body.classList.add('fixed');
+	}
 
-// 	const closeMenu = () => {
-// 		mobileMenuPopup?.classList.remove('active');
-// 		document.body.classList.remove('fixed');
-// 	}
+	const closeMenu = () => {
+		mobileMenuPopup.classList.remove('active');
+		document.body.classList.remove('fixed');
+	}
 
-// 	return {
-// 		open: openMenu,
-// 		close: closeMenu
-// 	}
-// }
+	return {
+		open: openMenu,
+		close: closeMenu
+	}
+}
